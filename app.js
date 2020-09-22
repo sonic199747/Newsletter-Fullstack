6dcf88fb3b3aaf5573573e127ba382054fb655fd
Sonic <sonic199747@gmail.com>
2020-09-22 16:16:14 -0600

    heroku

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post("/", function (req, res) {
 app.post("/failure", function (req, res) {
   res.redirect("/");
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
 
